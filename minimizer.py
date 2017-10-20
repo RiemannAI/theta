0e03fd3efa17e737e86ab4309b3ba31215e79154
Stefano Carrazza <stefano.carrazza@cern.ch>
2017-10-20 16:46:18 +0200

    implemeting schur complement idea

--- minimizer.py
+++ minimizer.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from cma import CMAEvolutionStrategy
-import numpy as np
 import multiprocessing as mp
 from contextlib import closing
-from rtbm import AssignError
 
 
 class Resource(object):
@@ -23,13 +21,8 @@ def worker_initialize(cost, model, x_data, y_data):
 
 
 def worker_compute(params):
-    try:
-        resource.model.set_parameters(params)
-    except AssignError:
-        return np.inf
+    resource.model.set_parameters(params)
     res = resource.cost_function(resource.model(resource.x_data), resource.y_data)
-    if np.isnan(res):
-        res = np.inf
     return res
 
 
@@ -46,12 +39,12 @@ class CMA(object):
     def train(self, cost, model, x_data, y_data=None, tolfun=1e-11, popsize=None, maxiter=None):
         """The training algorithm"""
 
-        bmin, bmax = model.get_bounds()
-        args = {'bounds': [bmin, bmax],
+        initsol = model.get_parameters()
+        args = {'bounds': [ [-model.param_bound]*len(initsol),
+                            [model.param_bound]*len(initsol)],
                 'tolfun': tolfun,
                 'verb_log': 0}
-        sigma = np.max(bmax)*0.1
-        initsol = np.real(model.get_parameters())
+        sigma = model.param_bound*0.1
 
         if popsize is not None:
             args['popsize'] = popsize
--- rtbm.py
+++ rtbm.py
@@ -17,7 +17,7 @@ class RTBM(object):
         Probability = 0
         Expectation = 1
 
-    def __init__(self, visible_units, hidden_units, mode=Mode.Probability, fraction=0.25):
+    def __init__(self, visible_units, hidden_units, mode=Mode.Probability):
         """Setup operators for BM based on the number of visible and hidden units"""
         self._Nv = visible_units
         self._Nh = hidden_units
@@ -26,15 +26,17 @@ class RTBM(object):
         self._bh = np.zeros([hidden_units, 1], dtype=complex)
         self._w = np.zeros([visible_units, hidden_units], dtype=complex)
         self._q = np.zeros([hidden_units, hidden_units], dtype=complex)
-        self._Np = 2*self._Nv + 2*self._Nh + self._Nv*self._Nh
+        self._a_size = (self._Nv+self._Nh)**2
+        self._a_shape = ((self._Nv+self._Nh), (self._Nv+self._Nh))
+        self._size = self._Nv + self._Nh + self._a_size
         self._mode = None
         self._call = None
-
-        # Set default parameter bound value
-        self._param_bound = 10
+        self.param_bound = 10
 
         # Populate with random parameters
-        self.random_init(fraction=fraction)
+        self._parameters = np.random.uniform(-self.param_bound,
+                                              self.param_bound, self._size)
+        self.set_parameters(self._parameters)
 
         # set operation mode
         self.mode = mode
@@ -45,89 +47,28 @@ class RTBM(object):
 
     def size(self):
         """Get size of RTBM"""
-        return self._Np
-
-    def get_bounds(self):
-        """Returns two arrays with min and max of each parameter for the GA"""
-        lower_bounds = [-self._param_bound for _ in range(self.size())]
-        upper_bounds = [ self._param_bound for _ in range(self.size())]
-
-        # set Q positive
-        index = self.size()-self._q.shape[0]
-        lower_bounds[index:] = [1E-5]*self._q.shape[0]
-
-        return lower_bounds, upper_bounds
+        return self._size
 
     def set_parameters(self, params):
         """Assigns a flat array of parameters to the RTBM matrices"""
-        if len(params) != self.size():
+        if len(params) != self._size:
             raise Exception('Size does no match.')
 
-        index = self._bv.shape[0]
-        bv = params[0:index].reshape(self._bv.shape)
+        self._parameters = params
+        x = params[0:self._a_size].reshape(self._a_shape)
+        a = np.transpose(x).dot(x)
+        self._q = a[:self._Nh,:self._Nh]
+        self._t = a[self._Nh:self._Nh+self._Nv,self._Nh:]
+        self._w = a[self._Nh:,:self._Nh]
+        self._bv = params[self._a_size:self._a_size+self._Nv].reshape(self._bv.shape)
+        self._bh = params[-self._Nh:].reshape(self._bh.shape)
 
-        t = np.diag(params[index:index+self._t.shape[0]])
-        index += self._t.shape[0]
-
-        bh = params[index:index+self._bh.shape[0]].reshape(self._bh.shape)
-        index += self._bh.shape[0]
-
-        w = params[index:index+self._w.size].reshape(self._w.shape)
-        index += self._w.size
-
-        q = np.diag(params[index:index+self._q.shape[0]])
-        
-        # Only keep if consistent solution
-        # Temporary work-around
-        if check_normalization_consistency(t, q, w):
-            self._w = w
-            self._q = q
-            self._t = t
-            self._bv = bv
-            self._bh = bh
-        else:
-            raise AssignError('RTBM assign_params: check normalization consistency failed')
+        if not check_normalization_consistency(self._t, self._q, self._w):
+            raise AssignError('not positive random initialization')
 
     def get_parameters(self):
         """Return flat array with current matrices weights"""
-        params = np.concatenate([self._bv.flatten(), self._t.diagonal(), self._bh.flatten(),
-                                 self._w.flatten(), self._q.diagonal()])
-        return params
-        
-    def random_init(self, t_max=2, q_max=5, w_max=2, fraction=0.25):
-        """ Initalizes the RTBM parameters uniform random
-        (the Bs are kept @ 0)"""
-
-        if self._Nv >= self._Nh:
-            # Init with positivity condition
-            self._q = np.diag(np.random.uniform(0.01, q_max, self._Nh))
-            self._t = np.diag(np.random.uniform(0.01, t_max, self._Nv))
-            w = np.zeros([self._Nv, self._Nh], dtype=complex)
-
-            # populate w until 25% is non zero
-            while np.count_nonzero(w) / float(w.size) <= fraction:
-                inserted = False
-                while not inserted:
-                    index = np.random.choice(self._w.size, 1)
-                    if w.flat[index] == 0:
-                        w.flat[index] = np.random.uniform(-w_max, w_max, 1)
-                        inserted = True
-
-                if not check_normalization_consistency(self._t, self._q, w):
-                    w.flat[index] = 0
-
-            self._w = w
-
-        else:
-            # Init random diagonal pos. def.
-            self._t = np.diag(np.random.uniform(0.01, t_max, self._Nv)).astype(complex)
-            self._q = np.diag(np.random.uniform(0.01, q_max, self._Nh)).astype(complex)
-
-            # Init random
-            self._w = np.random.uniform(-w_max, w_max, (self._Nv, self._Nh)).astype(complex)
-
-            while not check_normalization_consistency(self._t, self._q, self._w):
-                self._w = np.random.uniform(-w_max, w_max, (self._Nv, self._Nh))
+        return self._parameters
 
     @property
     def mode(self):
