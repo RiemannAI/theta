63529f0886ca86e56ffaf82a8057bb460eb0b7f1
Stefano Carrazza <stefano.carrazza@cern.ch>
2019-06-19 15:42:13 +0200

    implemeting RTBM sampling

--- theta/mathtools.py
+++ theta/mathtools.py
@@ -27,12 +27,12 @@ def rtbm_parts(v, bv, bh, t, w, q, mode=1):
     BiTB = np.dot(np.dot(BvT, invT), bv)
     BtiTW = np.dot(np.dot(BvT, invT), w)
     WtiTW = np.dot(np.dot(w.T, invT), w)
-    
+
     ExpF = np.exp(-0.5 * vTv.diagonal() - Bvv - BiTB * np.ones(v.shape[1]))
-    
+
     uR1, vR1 = RiemannTheta.parts_eval((vT.dot(w) + BhT) / (2.0j * np.pi), -q / (2.0j * np.pi), mode, epsilon=RTBM_precision)
     uR2, vR2 = RiemannTheta.parts_eval((BhT - BtiTW) / (2.0j * np.pi), (-q + WtiTW) / (2.0j * np.pi), mode, epsilon=RTBM_precision)
-   
+
     return ( np.sqrt(detT / (2.0 * np.pi) ** (v.shape[0])) * ExpF ), ( vR1 / vR2 * np.exp(uR1-uR2) )
 
 
@@ -72,7 +72,7 @@ def rtbm_log_probability(v, bv, bh, t, w, q, mode=1):
     WtiTW = np.dot(np.dot(w.T, invT), w)
 
     ExpF = -0.5 * vTv.diagonal() - Bvv - BiTB * np.ones(v.shape[1])
-  
+
     lnR1 = RiemannTheta.log_eval((vT.dot(w) + BhT) / (2.0j * np.pi), -q / (2.0j * np.pi), mode, epsilon=RTBM_precision)
     lnR2 = RiemannTheta.log_eval((BhT - BtiTW) / (2.0j * np.pi), (-q + WtiTW) / (2.0j * np.pi), mode, epsilon=RTBM_precision)
 
@@ -124,7 +124,7 @@ def gradient_log_1d_theta_phaseII(v, q, d):
 
     R = RiemannTheta(v / (2.0j * np.pi), -q / (2.0j * np.pi), mode=2, epsilon=RTBM_precision)
     L = RiemannTheta(v / (2.0j * np.pi), -q / (2.0j * np.pi), mode=2, epsilon=RTBM_precision, derivs=[D])
-   
+
     return (-(L/R) / (2.0j * np.pi)).flatten()
 
 
@@ -136,18 +136,18 @@ def theta_1d(v, q, d):
     # Cutoff if q is not positive definite
     if(np.real(q[0,0])<=0):
         q[0,0] = 1e-5
-            
+
     if(d > 0):
         D = np.ones((1,d))
-        
+
         R = 1.0/((2j*np.pi)**d) * RiemannTheta(v / (2.0j * np.pi), -q / (2.0j * np.pi), epsilon=RTBM_precision, derivs=D)
     else:
-        
+
         R = RiemannTheta(v / (2.0j * np.pi), -q / (2.0j * np.pi), epsilon=RTBM_precision)
-  
+
     return R
-    
-    
+
+
 def logtheta_1d_phaseI(v, q, d):
     """ Wraps the RT for 1d subcase with dth order directional gradient
 
@@ -155,21 +155,21 @@ def logtheta_1d_phaseI(v, q, d):
     """
     # Cutoff if q is not positive definite
     if(np.real(q[0,0])<=0 or np.isnan(q).any()):
-        print("NaN detected or negative value in phase I: ",q)   
+        print("NaN detected or negative value in phase I: ",q)
         q[0,0] = np.abs(q[0,0])
-            
+
     if(d > 0):
         D = np.ones((1,d))
-        
+
         R = -d*np.log(((2j*np.pi))) + RiemannTheta.log_eval(v / (2.0j * np.pi), -q / (2.0j * np.pi), mode=1, epsilon=RTBM_precision, derivs=D)
-        
+
     else:
         # Make NaN safe via moving to fundamental box
         re = np.divmod(v, q)
-      
+
         e = (np.asarray(re[0])*v -0.5*q[0,0]*np.asarray(re[0])**2)[:,0]
         R = e + RiemannTheta.log_eval(re[1] / (2.0j * np.pi), -q / (2.0j * np.pi), mode=1, epsilon=RTBM_precision)
-       
+
     return R
 
 
@@ -180,17 +180,17 @@ def logtheta_1d(v, q, d):
     """
     # Cutoff if q is not positive definite
     if(np.real(q[0,0])<=0 or np.isnan(q).any()):
-        print("NaN detected or negative value in phase I: ",q)   
+        print("NaN detected or negative value in phase I: ",q)
         q[0,0] = np.abs(q[0,0])
-            
+
     if(d > 0):
         D = np.ones((1,d))
-        
+
         R = -d*np.log(((2j*np.pi))) + RiemannTheta.log_eval(v / (2.0j * np.pi), -q / (2.0j * np.pi), epsilon=RTBM_precision, derivs=D)
-        
+
     else:
         R = RiemannTheta.log_eval(v / (2.0j * np.pi), -q / (2.0j * np.pi), epsilon=RTBM_precision)
-       
+
     return R
 
 
@@ -205,7 +205,7 @@ def hidden_expectations(v, bh, w, q):
 
     E = np.zeros((Nh,v.shape[1]), dtype=complex)
     vWbhT = vW + bh.T
-    
+
     for i in range(0, Nh):
         E[i] = gradient_log_theta(vWbhT, q, i)
 
@@ -218,19 +218,19 @@ def factorized_hidden_expectations(vWb, q, mode=1):
 
         Returns [ E(h_1|v), E(h_2|v), ... ] in vectorized form (each E is an array for the vs)
     """
-    
+
     Nh = q.shape[0]
 
     E = np.zeros((Nh,vWb.shape[0]), dtype=complex)
-    
+
     for i in range(Nh):
         O = np.matrix([[q[i, i]]], dtype=complex)
-        
+
         # Cutoff to keep positive definite
         if(np.real(O[0,0])<=0 or np.isnan(O).any()):
-            print("NaN detected or negative value: ",O)   
+            print("NaN detected or negative value: ",O)
             O[0,0] = np.abs(O[0,0])
-        
+
         E[i] = -1.0/(2j*np.pi)*RiemannTheta.normalized_eval(vWb[:, [i]] / (2.0j * np.pi), -O/ (2.0j * np.pi), mode=mode, epsilon=RTBM_precision, derivs=[[1]])
 
     return E
@@ -239,12 +239,25 @@ def factorized_hidden_expectations(vWb, q, mode=1):
 
 def factorized_hidden_expectation_backprop(vWb, q, mode=1):
     Tn = np.zeros((3, vWb.shape[1], vWb.shape[0]), dtype=complex)
-    
-    for i in range(0, vWb.shape[1]): 
+
+    for i in range(0, vWb.shape[1]):
         O = np.matrix([[q[i, i]]], dtype=complex)
-       
+
         Tn[:,i,:]  =  RiemannTheta.normalized_eval(vWb[:,[i]] / (2.0j * np.pi) , -O/ (2.0j * np.pi), mode=mode, derivs=np.array( [ [1], [1,1], [1,1,1] ]  ) , epsilon=RTBM_precision  )
-    
-    
-    return Tn 
-    
+
+
+    return Tn
+
+
+def rtbm_ph(model, h):
+    invT = np.linalg.inv(model.t)
+    WtiTW = np.dot(np.dot(model.w.T, invT), model.w)
+    QWTW = model.q - WtiTW
+    hTQWTWh = np.dot(np.dot(h.T, QWTW), h)
+    BtiTW = np.dot(np.dot(model.bv.T, invT), model.w)
+    BhT = model.bh.T
+    BBTW = BhT - BtiTW
+    BBTWh = np.dot(BBTW ,h)
+    ExpF = np.exp(-0.5*hTQWTWh-BBTWh)
+    u, v = RiemannTheta.parts_eval((BhT-BtiTW)/(2j*np.pi),(-model.q+WtiTW)/(2j*np.pi), mode=1, epsilon=RTBM_precision)
+    return ExpF / v * np.exp(-u)
